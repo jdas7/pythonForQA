228c260d9364a98c8996369768eed84317351ebc
Juan David Alvis <juanalvis6@gmail.com>
2024-02-10 20:17:07 -0500

    funtion input

---
+++ basicPython/3_function_input.py
@@ -0,0 +1,14 @@
+# input
+# the default input function returns a string
+
+result = input("enter your name: ")
+
+print("your name is: ", result)
+
+# operations with numbers
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+result = num1 + num2
+print("the result is: ", result)
