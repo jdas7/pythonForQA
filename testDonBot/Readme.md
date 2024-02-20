77688eaea35cb357e9d1ea16dd927caea1fa63c7
Juan David Alvis <juanalvis6@gmail.com>
2024-02-20 13:29:00 -0500

    test_api

---
+++ testDonBot/Readme.md
@@ -0,0 +1,11 @@
+# Example DONBOT
+
+Para usar DonBot debes tener allure instalado ya que los reportes de DonBot se trabajan con allure
+
+usa el comando allure --version para verificar que lo tengas instalado
+
+para instalar donbot debes usar el comando:
+pip install git+https://github.com/jdas7/DonBot.git 
+
+
+
--- testDonBot/features/steps/test_api_steps.py
+++ testDonBot/features/steps/test_api_steps.py
@@ -1,19 +1,16 @@
 from donbot.requests_utils import RequestsUtils
-from behave import given, when, then
-
-response = None
+from behave import given, then
 
 
 @given('who made a GET request to "https://pokeapi.co/api/v2/"')
 def step_impl(context):
     url = "https://pokeapi.co/api/v2/"
-    global response
-    response = RequestsUtils.send_get_request(url)
+    context.response = RequestsUtils.send_get_request(url)
 
 
 @then('the response must have status code 200')
 def step_impl(context):
-    assert response is not None, "No se ha realizado la solicitud GET"
-    assert response.status_code == 200, f"La solicitud GET tuvo un código de estado inesperado: {response.status_code}"
+    assert context.response is not None, "No se ha realizado la solicitud GET"
+    assert context.response.status_code == 200, f"La solicitud GET tuvo un código de estado inesperado: {context.response.status_code}"
     print("Petición GET exitosa:")
-    print(response.text)
+    print(context.response.text)
